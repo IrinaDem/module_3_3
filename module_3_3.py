bb4a24cc9e046505cf66d9fe6e96e29e67706e7d
IrinaDem <starshinova9297@gmail.com>
2024-10-28 15:58:10 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -17,7 +17,7 @@ print_params(**values_dict)
 
 print("\nРаспаковка + отдельные параметры:")
 values_list_2 = [54.32, 'Строка']
-print_params(*values_list_2, 42)
+print_params(*values_list_2, c=42)
 
 def append_to_list(item, list_my=None):
     if list_my is None:
@@ -25,12 +25,7 @@ def append_to_list(item, list_my=None):
     list_my.append(item)
     return list_my
 
-print("\nПримеры использования append_to_list:")
-result1 = append_to_list(1)
-print(result1)
-
-result2 = append_to_list(2)
-print(result2)
-
-result3 = append_to_list(3, result1)
-print(result3) 
+my_list = append_to_list(1)
+my_list = append_to_list(2, my_list)
+my_list = append_to_list(3)
+print("\nСодержимое списка:", my_list)
